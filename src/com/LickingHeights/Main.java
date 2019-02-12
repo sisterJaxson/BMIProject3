ad46beda317313726faff8413f4227197f931fc1
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-12 13:54:23 -0500

    Made scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,10 +1,21 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("wow");
         //don't mind above line of code. It was a test.
+        String userName;
+        Scanner keyboard;
+
+
+        Scanner = new Scanner(System.in);
+
+
+
+        
 
 
 
