0bf8c195aaeba1e9bcc24cbb8ec1a64e9f4e9251
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-12 14:04:56 -0500

    Greeted the user

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("wow");
+        //System.out.println("wow");
         //don't mind above line of code. It was a test.
         String userName;
         Scanner keyboard;
 
 
-        Scanner = new Scanner(System.in);
+        keyboard = new Scanner(System.in);
 
 
+        System.out.println("Hello! What is your name? ");
+        userName = keyboard.nextLine();
+        System.out.println("How are you?");
+        userName = keyboard.nextLine();
+        System.out.println("How tall are you? In inches.");
+        userName = keyboard.nextLine();
+        System.out.println("How much do you weigh? In pounds.");
+        userName = keyboard.nextLine();
 
-        
+
 
 
 
