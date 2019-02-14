eb9be5b402e3afc37dfa041b1175e1232f767ca2
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-02-14 14:01:53 -0500

    Re did everything

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,23 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.println("wow");
-        //don't mind above line of code. It was a test.
         String userName;
         Scanner keyboard;
+        double weight;
+        int feet;
+        int inches;
 
 
         keyboard = new Scanner(System.in);
 
+        System.out.println("What is your weight in pounds: ");
+        weight = keyboard.nextFloat();
 
-        System.out.println("Hello! What is your name? ");
-        userName = keyboard.nextLine();
-        System.out.println("Hi "+userName+" How are you?");
-        userName = keyboard.nextLine();
-        System.out.println("How tall are you? In inches.");
-        userName = keyboard.nextLine();
-        System.out.println("How much do you weigh? In pounds.");
-        userName = keyboard.nextLine();
+        System.out.println("What is your height. Feet: ");
+        feet = keyboard.nextInt();
+
+        System.out.println("Inches: ");
+        inches = keyboard.nextInt();
+
+        double weightInKilos = weight*0.453592;
+        double heightInMeters = (((feet*12)+inches)*0.0254);
+        double bmi = weightInKilos / Math.pow(heightInMeters, 2.0);
+
+        System.out.println("Your BMI is: "+bmi);
+
+
+
+
+
+
+
+
 
 
 
